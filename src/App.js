67eb779167d05369425cc7a0455f95163a829a0e
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-04-17 03:06:56 +0600

    Complete tutorial 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-// import HookCounter from './components/HookCounter';
-import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
 
 function App() {
   return (
     <div className="App">
-      {/* <HookCounter/> */}
-      <HookCounterTwo/>
+      <HookCounterThree/> 
+
     </div>
   );
 }
