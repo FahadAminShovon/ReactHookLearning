08a9b9aeec7adef75dd8c4134182443b4da1d483
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-04-18 02:32:30 +0600

    Complete useEffect tutorial 2

--- src/components/UseEffect/HookCounterOne.js
+++ src/components/UseEffect/HookCounterOne.js
@@ -3,13 +3,19 @@ import React, {useState, useEffect} from 'react';
 const HookCounterOne = () => {
 
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
 
     useEffect(() => {
+        console.log("the document title is changing")
         document.title = `you clicked ${count} times`
-    })
+    },[count])
 
     return ( 
         <div>
+            <input type = "text"
+                value = {name}
+                onChange = {e => setName(e.target.value)}
+            />
             <button onClick = {()=>setCount(prevCount => prevCount + 1)}>Count: {count}</button>
         </div>
      );
