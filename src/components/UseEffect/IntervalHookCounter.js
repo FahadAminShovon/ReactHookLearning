efd6dbe5c865849d6d53a2e3c885a40c60ea9e86
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-04-18 03:33:52 +0600

    Tip on prop dependency

--- src/components/UseEffect/IntervalHookCounter.js
+++ src/components/UseEffect/IntervalHookCounter.js
@@ -4,12 +4,20 @@ const IntervalHookCounter = () => {
 
     const [count, setCount] = useState(0)
 
+
+
     useEffect( () =>{
+
+    //   function doSomething () {
+    //       console.log(someProp)
+    //   }  
+    //   doSomething()
+
       const interval = setInterval(tick, 1000)
       return () => {
           clearInterval(interval)
       }
-    },[])
+    },[/*someProp*/])
 
     const tick = () => {
         setCount(prevCount => prevCount + 1)
