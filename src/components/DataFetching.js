5458223a25305404b709a8db45cc24f3302d6c97
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-04-18 21:23:57 +0600

    Datafetching 2

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -4,26 +4,27 @@ import axios from 'axios';
 
 const DataFetching = () => {
 
-    const [posts, setPosts] = useState([])
+    const [post, setPost] = useState({})
+    const [id, setId] = useState(1)
 
     useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => {
                 console.log(res)
-                setPosts(res.data)
+                setPost(res.data)
             })
             .catch(err => {
                 console.log(err.responst)
             })
-    }, [])
+    }, [id])
 
     return ( 
         <div>
-            <ul>
-                {posts.map(post => 
-                    <li key= {post.id}>{post.title}</li>
-                )}
-            </ul>
+            <input type = "text"
+                value = {id}
+                onChange = {e => setId(e.target.value)}
+            />
+            <h1>{post.title}</h1>
         </div>
      );
 }
