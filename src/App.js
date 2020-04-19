755943689caa21fbe9ffbd1f43c132c7cb0afe44
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-04-20 00:30:06 +0600

    Complex state & action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import CounterOne from './components/UseReducer/CounterOne';
+import CounterTwo from './components/UseReducer/CounterTwo';
 
 
 function App() {
   return (
     <div className="App">
-      <CounterOne/>
+      <CounterTwo/>
     </div>
   );
 }
---
+++ src/components/UseReducer/CounterTwo.js
@@ -0,0 +1,43 @@
+import React, {useReducer} from 'react';
+
+const initialState = {
+    firstCounter: 0,
+    secondCounter: 10,
+}
+const reducer = (state , action) => {
+    switch(action.type){
+        case 'increment':
+            return {...state, firstCounter: state.firstCounter + action.value}
+        case 'decrement':
+            return {...state, firstCounter: state.firstCounter - action.value}
+        case 'increment2':
+            return {...state, secondCounter: state.secondCounter + action.value}
+        case 'decrement2':
+            return {...state, secondCounter: state.secondCounter - action.value}
+        case 'reset':
+            return action.value
+        default:
+            return state
+    }
+}
+
+const CounterTwo = () => {
+    const [count, dispatch] = useReducer(reducer, initialState)
+
+    return ( 
+        <div>
+            <div>Count - {count.firstCounter}</div>
+            <div>Count - {count.secondCounter}</div>
+
+            <button onClick = {() => dispatch({type: 'increment', value: 1})}>Increment</button>
+            <button onClick = {() => dispatch({type: 'decrement', value: 1})}>Decrement</button>
+            <button onClick = {() => dispatch({type: 'increment2', value: 1})}>Increment2</button>
+            <button onClick = {() => dispatch({type: 'decrement2', value: 1})}>Increment2</button>
+            <button onClick = {() => dispatch({type: 'increment', value: 5})}>Increment 5</button>
+            <button onClick = {() => dispatch({type: 'decrement', value: 5})}>Decrement 5</button>
+            <button onClick = {() => dispatch({type: 'reset', value: initialState})}>Reset</button>
+        </div>
+     );
+}
+ 
+export default CounterTwo;
