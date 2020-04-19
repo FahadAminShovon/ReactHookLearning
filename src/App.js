d8dd001cb2a52a8158d8728907a6c6c23d8e326f
Fahad Amin Shovon <bm.amin.fahad@gmail.com>
2020-04-20 00:09:33 +0600

    Simple useReducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import ComponentE from './components/Context/ComponentE';
+import CounterOne from './components/UseReducer/CounterOne';
 
-export const UserContext = React.createContext()
-export const ChannelContext = React.createContext()
+
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value = {"Fahad"}>
-        <ChannelContext.Provider value = {"Codevolution"}>
-          <ComponentE/>
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      <CounterOne/>
     </div>
   );
 }
